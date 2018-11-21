0371ad203e7e52980999691812f845c4d5760331
Ikko Yamane <yamane.ikko@gmail.com>
2018-11-22 01:22:08 +0900

    Fix a typo and clean import statements.

--- uplift/with_joint_labels/__init__.py
+++ uplift/with_joint_labels/__init__.py
@@ -1,9 +1,15 @@
-from ._uplift_svm import UpliftSVMThreshold, UpliftRankSVM
-from ._uplift_logistic import UpliftLogisticRegression, LogisticSeparate, TwoLogistic
-from ._uplift_ramp import UpliftRampThresholdSGDManyTimes, UpliftSigCV, UpliftRampThresholdSGD, Net33
+from ._uplift_svm import UpliftSVMThreshold
+from ._uplift_svm import UpliftRankSVM
+from ._uplift_logistic import UpliftLogisticRegression
+from ._uplift_logistic import LogisticSeparate
+from ._uplift_logistic import TwoLogistic
+from ._uplift_ramp import UpliftRampThresholdSGDManyTimes
+from ._uplift_ramp import UpliftSigCV
+from ._uplift_ramp import UpliftRampThresholdSGD
+from ._uplift_ramp import Net33
 from ._uplift_random import UpliftRandom
-from ._uplift_least_squres import UpliftLS
-from ._uplift_least_squres import UpliftLSCV
+from ._uplift_least_squares import UpliftLS
+from ._uplift_least_squares import UpliftLSCV
 from ._util import calc_AUUC
 from ._util import calc_uplift
 from ._util import max_uplift

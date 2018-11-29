edf9748baf83d8300e177243990131354544239d
Ikko Yamane <yamane.ikko@gmail.com>
2018-11-29 22:31:15 +0900

    Remove unstable code.

--- uplift/with_separate_labels/__init__.py
+++ uplift/with_separate_labels/__init__.py
@@ -5,11 +5,4 @@ from ._four_logistic_regressors import FourLogistic
 from ._four_ridge_regressors import FourRidgeRegressors
 from ._four_neural_nets import FourNNRegressors
 from ._four_neural_nets import FourNNClassifiers
-from ._two_logistic_regressors import TwoLogisticRegressors
-from ._two_neural_nets import TwoNeuralNets
 from ._minmax_linear import MinMaxLinear
-from ._minmax_neural_nets import MinMaxNeuralNets
-from ._minmax_neural_nets import MinMaxLayered
-from ._minmax_neural_net_with_linear import MinMaxNNwithLin
-from ._minmax_model_selection import GridSearchMinMax
-from ._min_grad_norm import MinGradNormNeuralNet
